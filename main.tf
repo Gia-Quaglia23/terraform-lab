54fea23e744f91006334550daa12265cc81bd874
Gia-Quaglia23 <111260932+Gia-Quaglia23@users.noreply.github.com>
2024-08-05 10:44:57 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "s3_buckets" {
+  source = "./modules/s3-bucket"
+  bucket_count = var.bucket_count
+  lifecycle_rules = var.lifecycle_rules
+}
 
